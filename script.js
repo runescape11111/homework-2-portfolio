c1e8624abd4ef319c9782cf1c2a732b187c88234
Oliver Shih <olivershih@gmail.com>
2021-04-03 22:01:35 -0700

    updated portfolio with project and links

---
+++ script.js
@@ -0,0 +1,15 @@
+var project = document.getElementById("first-child");
+var github = document.getElementById("github");
+var linkedin = document.getElementById("linkedin");
+
+project.addEventListener("click", function(){
+    var tab = window.open("https://irvinek01.github.io/project-1-secret-dictionary/", "_blank");
+});
+
+github.addEventListener("click", function(){
+    var tab = window.open("https://github.com/runescape11111", "_blank");
+});
+
+linkedin.addEventListener("click", function(){
+    var tab = window.open("https://www.linkedin.com/in/oliver-shih-6742ab79/", "_blank");
+});
